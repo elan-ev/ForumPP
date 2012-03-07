2a2ffdd8b7ef4edbe01963e9fe1cc9b8c4d7520e
tgloeggl <tgloeggl@8b9782fd-b62b-0410-a6c6-fea12034b7c6>
2012-03-07 13:33:38 +0000

    ForumPP: make jslint happy, fixes the js

--- javascript/application.js
+++ javascript/application.js
@@ -1,3 +1,4 @@
+/*global window, $, jQuery, document, _ */
 /* ------------------------------------------------------------------------
  * the global STUDIP namespace
  * ------------------------------------------------------------------------ */
@@ -15,9 +16,9 @@ STUDIP.ForumPP = {
         STUDIP.ForumPP.initSmileys();
     },
 
-    initAreas: function() {
+    initAreas: function () {
         // bind click events on add-area at bottom row of each category
-        jQuery('div.add_area').bind('click', function() {
+        jQuery('div.add_area').bind('click', function () {
             STUDIP.ForumPP.addArea(this);
         });
 
@@ -26,10 +27,10 @@ STUDIP.ForumPP = {
             axis: 'y',
             items: ">*.movable",
             handle: 'td.handle',
-            stop: function() {
+            stop: function () {
                 var categories = {};
                 categories.categories = {};
-                jQuery(this).find('table').each(function() {
+                jQuery(this).find('table').each(function () {
                     var name = jQuery(this).attr('data-category-id');
                     categories.categories[name] = name;
                 });
@@ -39,31 +40,31 @@ STUDIP.ForumPP = {
                     url: STUDIP.URLHelper.getURL('plugins.php/forumpp/index/savecats?cid=' + STUDIP.ForumPP.seminar_id),
                     data: categories
                 });
-            }            
+            }
         });
-        
+
         jQuery('tbody.sortable').sortable({
             axis: 'y',
             items: ">*:not(.sort-disabled)",
             connectWith: 'tbody.sortable',
             handle: 'div.handle',
-            helper: function(e, ui) {
-                ui.children().each(function() {
+            helper: function (e, ui) {
+                ui.children().each(function () {
                     jQuery(this).width(jQuery(this).width());
                 });
                 return ui;
             },
-            
-            stop: function() {
+
+            stop: function () {
                 // iterate over each category and get the areas there
                 var areas = {};
                 areas.areas = {};
-                jQuery('#sortable_areas').find('table').each(function() {
+                jQuery('#sortable_areas').find('table').each(function () {
                     var category_id = jQuery(this).attr('data-category-id');
-                    
+
                     areas.areas[category_id] = {};
-                    
-                    jQuery(this).find('tr').each(function() {
+
+                    jQuery(this).find('tr').each(function () {
                         var area_id = jQuery(this).attr('data-area-id');
                         areas.areas[category_id][area_id] = area_id;
                     });
@@ -74,30 +75,30 @@ STUDIP.ForumPP = {
                     url: STUDIP.URLHelper.getURL('plugins.php/forumpp/index/saveareas?cid=' + STUDIP.ForumPP.seminar_id),
                     data: areas
                 });
-            }            
+            }
         });
-        
+
         // compile template
         STUDIP.ForumPP.deleteAreaTemplate     = _.template(jQuery('#question_delete_area').text());
         STUDIP.ForumPP.deleteCategoryTemplate = _.template(jQuery('#question_delete_category').text());
     },
-    
-    initSmileys: function() {
-        jQuery('.smiley_favorites img').bind('click', function(){
+
+    initSmileys: function () {
+        jQuery('.smiley_favorites img').bind('click', function () {
             jQuery('#inhalt').insertAtCaret(jQuery(this).attr('data-smiley'));
         });
     },
 
-    approveDelete: function() {
+    approveDelete: function () {
         if (STUDIP.ForumPP.current_area_id) {
             // hide the area in the dom
-            jQuery('tr[data-area-id='+ STUDIP.ForumPP.current_area_id +']').remove();
+            jQuery('tr[data-area-id=' + STUDIP.ForumPP.current_area_id + ']').remove();
             jQuery('#question').hide();
 
             // ajax call to make the deletion permanent
             jQuery.ajax(STUDIP.URLHelper.getURL('plugins.php/forumpp/index/delete_entry/'
                 + STUDIP.ForumPP.current_area_id + '?cid=' + STUDIP.ForumPP.seminar_id), {
-                success: function(html) {
+                success: function (html) {
                     jQuery('#message_area').html(html);
                 }
             });
@@ -107,18 +108,18 @@ STUDIP.ForumPP = {
 
         if (STUDIP.ForumPP.current_category_id) {
             // hide the table in the dom
-            jQuery('table[data-category-id='+ STUDIP.ForumPP.current_category_id +']').fadeOut();
+            jQuery('table[data-category-id=' + STUDIP.ForumPP.current_category_id + ']').fadeOut();
             jQuery('#question').hide();
 
             // move all areas to the default category
-            jQuery('table[data-category-id='+ STUDIP.ForumPP.current_category_id +'] tr.movable').each(function() {
-               jQuery('table[data-category-id=' + STUDIP.ForumPP.seminar_id + ']').append(jQuery(this));
+            jQuery('table[data-category-id=' + STUDIP.ForumPP.current_category_id + '] tr.movable').each(function () {
+                jQuery('table[data-category-id=' + STUDIP.ForumPP.seminar_id + ']').append(jQuery(this));
             });
 
             // ajax call to make the deletion permanent
             jQuery.ajax(STUDIP.URLHelper.getURL('plugins.php/forumpp/index/remove_category/'
                 + STUDIP.ForumPP.current_category_id + '?cid=' + STUDIP.ForumPP.seminar_id), {
-                success: function(html) {
+                success: function (html) {
                     jQuery('#message_area').html(html);
                 }
             });
@@ -127,31 +128,31 @@ STUDIP.ForumPP = {
         }
     },
 
-    disapproveDelete: function() {
+    disapproveDelete: function () {
         jQuery('#question').hide();
     },
 
-    deleteArea: function(element, area_id) {
+    deleteArea: function (element, area_id) {
         jQuery('#modalquestion').text(STUDIP.ForumPP.deleteAreaTemplate({
             area: jQuery(element).parent().parent().find('span.areaname').text()
         }));
 
         STUDIP.ForumPP.current_area_id = area_id;
-        
+
         jQuery('#question').show();
     },
 
-    addArea: function(element) {
+    addArea: function (element) {
         this.cancelAddArea();
         jQuery(element).hide().parent().find("form.add_area_form").show();
     },
 
-    cancelAddArea: function() {
+    cancelAddArea: function () {
         jQuery('form.add_area_form').hide();
         jQuery('div.add_area').show();
     },
 
-    deleteCategory: function(category_id, name) {
+    deleteCategory: function (category_id, name) {
         jQuery('#modalquestion').text(STUDIP.ForumPP.deleteCategoryTemplate({
             category: name
         }));
@@ -159,13 +160,13 @@ STUDIP.ForumPP = {
         STUDIP.ForumPP.current_category_id = category_id;
         jQuery('#question').show();
     },
-    
-    editCategoryName: function(category_id) {
+
+    editCategoryName: function (category_id) {
         jQuery('table[data-category-id=' + category_id + '] span.heading').hide();
         jQuery('table[data-category-id=' + category_id + '] span.heading_edit').show();
     },
 
-    cancelEditCategoryName: function(category_id) {
+    cancelEditCategoryName: function (category_id) {
         jQuery('table[data-category-id=' + category_id + '] span.heading_edit').hide();
         jQuery('table[data-category-id=' + category_id + '] span.heading').show();
 
@@ -174,8 +175,8 @@ STUDIP.ForumPP = {
             jQuery('table[data-category-id=' + category_id + '] span.category_name').text().trim()
         );
     },
-    
-    saveCategoryName: function(category_id) {
+
+    saveCategoryName: function (category_id) {
         var name = {};
         name.name = jQuery('table[data-category-id=' + category_id + '] span.heading_edit input[type=text]').val();
 
@@ -192,22 +193,22 @@ STUDIP.ForumPP = {
     },
 
 
-    editAreaName: function(area_id) {
+    editAreaName: function (area_id) {
         jQuery('tr[data-area-id=' + area_id + '] span.areaname').parent().hide();
         jQuery('tr[data-area-id=' + area_id + '] span.areaname_edit').show();
     },
 
-    cancelEditAreaName: function(area_id) {
+    cancelEditAreaName: function (area_id) {
         jQuery('tr[data-area-id=' + area_id + '] span.areaname_edit').hide();
         jQuery('tr[data-area-id=' + area_id + '] span.areaname').parent().show();
-        
+
         // reset the input field with the unchanged name
         jQuery('tr[data-area-id=' + area_id + '] span.areaname_edit input[type=text]').val(
             jQuery('tr[data-area-id=' + area_id + '] span.areaname').text().trim()
         );
     },
-    
-    saveAreaName: function(area_id) {
+
+    saveAreaName: function (area_id) {
         var name = {};
         name.name = jQuery('tr[data-area-id=' + area_id + '] span.areaname_edit input[type=text]').val();
 
@@ -222,19 +223,19 @@ STUDIP.ForumPP = {
             data: name
         });
     },
-    
-    moveThreadDialog: function(topic_id) {
+
+    moveThreadDialog: function (topic_id) {
         jQuery('#dialog_' + topic_id).dialog();
     },
-    
-    preview: function(text_element_id, preview_id) {
+
+    preview: function (text_element_id, preview_id) {
         var posting = {};
         posting.posting = jQuery('#' + text_element_id).val();
-        
+
         jQuery.ajax(STUDIP.URLHelper.getURL('plugins.php/forumpp/index/preview?cid=' + STUDIP.ForumPP.seminar_id), {
             type: 'POST',
             data: posting,
-            success: function(html) {
+            success: function (html) {
                 jQuery('#' + preview_id).html(html);
                 jQuery('#' + preview_id).parent().show();
             }
@@ -245,8 +246,8 @@ STUDIP.ForumPP = {
 
 // TODO: make TIC and add this to the Stud.IP-Core
 jQuery.fn.extend({
-    insertAtCaret: function(myValue){
-        return this.each(function(i) {
+    insertAtCaret: function (myValue) {
+        return this.each(function (i) {
             if (document.selection) {
                 //For browsers like Internet Explorer
                 this.focus();
@@ -258,7 +259,8 @@ jQuery.fn.extend({
                 var startPos = this.selectionStart;
                 var endPos = this.selectionEnd;
                 var scrollTop = this.scrollTop;
-                this.value = this.value.substring(0, startPos)+myValue+this.value.substring(endPos,this.value.length);
+                this.value = this.value.substring(0, startPos) + myValue
+                    + this.value.substring(endPos, this.value.length);
                 this.focus();
                 this.selectionStart = startPos + myValue.length;
                 this.selectionEnd = startPos + myValue.length;
